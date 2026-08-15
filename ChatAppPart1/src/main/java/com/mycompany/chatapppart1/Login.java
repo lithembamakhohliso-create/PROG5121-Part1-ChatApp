f8be53d12c79be7735b11ac29af3fd6cf6ecd158
lithembamakhohliso-create <lithembamakhohliso@gmail.com>
2026-08-15 12:57:36 +0200

    Update Login.java

--- ChatAppPart1/src/main/java/com/mycompany/chatapppart1/Login.java
+++ ChatAppPart1/src/main/java/com/mycompany/chatapppart1/Login.java
@@ -9,5 +9,67 @@ package com.mycompany.chatapppart1;
  * @author Student
  */
 public class Login {
+   
+    //Declere variables that are going to be used by the user
+    String username;
+    String password;
+    String phoneNumber;
     
-}
+    //Username  Validation
+    public boolean checkUserName(String username) {
+        return username.contains("_")&& username.length() <=5;
+        
+    }
+        
+        //Password validation
+        public boolean checkPasswordComplexity(String password) {
+            
+            boolean hasCapital = false;
+            boolean hasNumber = false;
+            boolean hasSpecial = false;
+            
+            for (int i = 0; i < password.length(); i++) {
+                char c = password.charAt(i);
+                
+                if (Character.isUpperCase(c)) {
+                    hasCapital = true;
+                } else if (Character.isDigit(c)) {
+                    hasNumber = true;
+                } else if (!Character.isLetterOrDigit(c)) {
+                    hasSpecial = true;
+                }
+            }
+            return password.length() >= 8 && hasCapital && hasNumber && hasSpecial;
+        }
+            //cellphone validation should start with +27
+            public boolean checkCellphoneNumber(String phone) {
+                return phone.startsWith("+27") && phone.length() <= 12;
+            }
+                
+            //Register Validation
+            
+            public String registerUser(String username, String password, String phoneNumber){
+                
+                if (!checkUserName(username)){
+                    return "Usename is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters in length.";
+                }
+                    
+                    
+                 if (!checkPasswordComplexity(password)) { 
+return "Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character."; 
+} 
+if (!checkCellphoneNumber(phoneNumber)) { 
+return "Cell phone number incorrectly formatted or does not contain international code."; 
+} 
+this.username = username; 
+this.password = password; 
+this.phoneNumber = phoneNumber; 
+return "User registered successfully.";   
+                    
+                    
+                    
+                }
+            }
+            
+            
+        
